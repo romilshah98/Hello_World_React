ddd58e460036f9b19a0982e59ccc66cea875358d
Romil <romilshah6462@gmail.com>
2020-02-01 17:39:50 -0800

    updated

---
+++ public/app.js
@@ -0,0 +1,34 @@
+var contentnode = document.getElementById('contents');
+
+class Home extends React.Component {
+    render() {
+        return React.createElement(
+            "div",
+            null,
+            React.createElement(
+                "h2",
+                null,
+                "Romil Shah"
+            ),
+            React.createElement("img", { src: "./romilphoto.jpg", alt: "Head Shot" }),
+            React.createElement(
+                "p",
+                null,
+                "I am Romil Shah. I am a Graduate student at San Diego State University with major in Computer Science. I am happy that I took Modern Web Development Frameworks in first semester and looking forward to this course."
+            ),
+            React.createElement("br", null),
+            React.createElement(
+                "button",
+                null,
+                " ",
+                React.createElement(
+                    "a",
+                    { href: "https://github.com/romilshah98/Assignment_1.git" },
+                    "VIEW MY GITHUB REPO"
+                )
+            )
+        );
+    }
+}
+
+ReactDOM.render(React.createElement(Home, null), contentnode);
---
+++ src/app.jsx
@@ -0,0 +1,18 @@
+var contentnode = document.getElementById('contents');
+
+class Home extends React.Component {
+    render() {
+        return (
+            <div>
+                <h2>Romil Shah</h2>
+                <img src="./romilphoto.jpg" alt="Head Shot"></img>
+                <p>
+                    I am Romil Shah. I am a Graduate student at San Diego State University with major in Computer Science. I am happy that I took Modern Web Development Frameworks in first semester and looking forward to this course.
+                </p><br/>
+                <button> <a href="https://github.com/romilshah98/Assignment_1.git">VIEW MY GITHUB REPO</a></button>
+            </div>
+        );
+    }
+}
+
+ReactDOM.render(<Home />, contentnode);
